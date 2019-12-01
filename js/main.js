fffa3bc2f81529f3213d301765d40620f55dff2e
Alanna Tai <alanna.tai@hotmail.com>
2019-12-01 02:07:25 -0500

    implement initial gameboard render and mine randomizer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 /*
-
 generate mines
 - certain amount of mines for a certain size of board 9x9 has 10mines
 - mines should not be generated in the same spot
@@ -30,16 +29,80 @@ right click number
 
 win
 - all cells that are not bombs are "flipped" or shown
-
 */
 
-const cell = {
-  id: row + col,
-  row: row,
-  column: column,
-  open: open,
-  flag: flag,
-  mine: mine,
-  adjMines: adjMines
+// class Cell {
+// 	constructor(row, column, id, open, flag, mine) {
+// 		this.row = row;
+// 		this.column = column;
+// 		this.id = `${row}${column}`;
+// 		this.open = false;
+// 		this.flag = false;
+// 		this.mine = false;
+// 	}
+// }
+
+// class Game {
+//   constructor (rows, columns, mines) {
+//     this.rows = 9;
+//     this.columns = 9;
+//     this.mines = 10;
+//     this.board = new Array(this.rows);
+//   }
+
+//     const cellEl = $('<div></div>').addClass('cell').attr('id');
+//     $('#board').append();
+//     console.log($('#board'))
+//     }
+
+// }
+
+let rows = 9;
+let columns = 9;
+let mineCount = 10;
+
+function createGameboard(length, value) {
+	return new Array(length).fill(value);
 }
 
+let gameboard = createGameboard(rows, null).map(() =>
+	createGameboard(columns, 0)
+);
+
+let cells = [];
+
+for (let i = 0; i < gameboard.length; i++) {
+	for (let j = 0; j < gameboard[i].length; j++) {
+		const cellEl = $('<div></div>').addClass('cell');
+    $('#board').append(cellEl.attr('id', `${i}${j}`));
+    cells.push([i,j])
+	}
+}
+
+console.log(cells.length)
+
+const generateRandom = function() {
+	return Math.floor(Math.random() * rows);
+};
+
+const generateMines = function() {
+	for (let i = 0; i < mineCount; i++) {
+		let rowIndex = generateRandom();
+    let columnIndex = generateRandom();
+    
+    cells.splice((cells.indexOf[rowIndex, columnIndex]), 1);
+
+		if (gameboard[rowIndex][columnIndex] !== 'mine') {
+			gameboard[rowIndex][columnIndex] = 'mine';
+			$(`#${rowIndex}${columnIndex}`).css('background-color', 'red');
+		} else {
+      gameboard[rowIndex][columnIndex] = 0;
+    }
+		console.log(rowIndex, columnIndex);
+	}
+};
+
+generateMines();
+console.log(cells)
+
+console.log(gameboard);
