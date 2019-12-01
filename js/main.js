cb4ceb50ad081436811b0339e3fbd72bcf20de9b
Alanna Tai <alanna.tai@hotmail.com>
2019-12-01 16:35:01 -0500

    Complete code that prints adjacentMineCount into cells. Needs hardcore refactoring...

--- js/main.js
+++ js/main.js
@@ -57,18 +57,21 @@ win
 
 // }
 
+//STATE
 let rows = 9;
 let columns = 9;
 let mineCount = 10;
 let cells = [];
 
+//CREATE BLANK GAMEBOARD FROM ROW/COLUMN SIZE
 function createGameboard(length, value) {
 	return new Array(length).fill(value);
 }
 let gameboard = createGameboard(rows, null).map(() =>
-	createGameboard(columns, 0)
+	createGameboard(columns, '0')
 );
 
+//RENDER GAMEBOARD INTO DOM
 for (let i = 0; i < gameboard.length; i++) {
 	for (let j = 0; j < gameboard[i].length; j++) {
 		const cellEl = $('<div></div>').addClass('cell');
@@ -77,6 +80,7 @@ for (let i = 0; i < gameboard.length; i++) {
 	}
 }
 
+//GENERATE RANDOM MINES INTO GAMEBOARD
 const generateMines = function() {
 	for (let i = 0; i < mineCount; i++) {
 		let randomMines = Math.floor(Math.random() * cells.length);
@@ -89,11 +93,88 @@ const generateMines = function() {
 
 generateMines();
 
-const generateAdjacent = function() {
-	for (let i = 0; i < gameboard.length; i++) {
-		for (let j = 0; j < gameboard[i].length; j++) {}
+//CHECK ADJACENT MINES
+let adjMineCount = 0;
+
+const checkN = function(cellRow, cellColumn) {
+	if (cellRow - 1 === -1) {
+		return;
+	} else if (gameboard[cellRow - 1][cellColumn] === 'mine') {
+		adjMineCount += 1;
 	}
 };
+const checkNE = function(cellRow, cellColumn) {
+	if (cellRow - 1 === -1 || cellColumn + 1 >= columns) {
+		return;
+	} else if (gameboard[cellRow - 1][cellColumn + 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkE = function(cellRow, cellColumn) {
+	if (cellColumn + 1 >= columns) {
+		return;
+	} else if (gameboard[cellRow][cellColumn + 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkSE = function(cellRow, cellColumn) {
+	if (cellRow + 1 >= rows || cellColumn + 1 >= columns) {
+		return;
+	} else if (gameboard[cellRow + 1][cellColumn + 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkS = function(cellRow, cellColumn) {
+	if (cellRow + 1 >= rows) {
+		return;
+	} else if (gameboard[cellRow + 1][cellColumn] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkSW = function(cellRow, cellColumn) {
+	if (cellRow + 1 >= rows || cellColumn - 1 === -1) {
+		return;
+	} else if (gameboard[cellRow + 1][cellColumn - 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkW = function(cellRow, cellColumn) {
+	if (cellColumn - 1 === -1) {
+		return;
+	} else if (gameboard[cellRow][cellColumn - 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+const checkNW = function(cellRow, cellColumn) {
+	if (cellRow - 1 === -1 || cellColumn - 1 === -1) {
+		return;
+	} else if (gameboard[cellRow - 1][cellColumn - 1] === 'mine') {
+		adjMineCount += 1;
+	}
+};
+
+//CALCULATE CELLS ADJACENT TO MINES
+const generateAdjacent = function() {
+	for (let r = 0; r < gameboard.length; r++) {
+    for (let c = 0; c < gameboard[r].length; c++) {
+			checkN(r, c);
+			checkNE(r, c);
+			checkE(r, c);
+			checkSE(r, c);
+			checkS(r, c);
+			checkSW(r, c);
+			checkW(r, c);
+      checkNW(r, c);
+      if (!(gameboard[r][c] === 'mine')) {
+        gameboard[r][c] = adjMineCount;
+        $(`#${r}${c}`).text(`${adjMineCount}`);
+      }
+      adjMineCount = 0;
+		}
+	}
+};
+
+generateAdjacent();
 
 console.log('cells:', cells);
 console.log('gameboard:', gameboard);
